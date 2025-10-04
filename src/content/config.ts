0ea89f79b48214ccd96143573ac57de09269675d
andrewsmyth09 <asmyth@gmx.com>
2025-10-04 21:21:43 +0200

    Warning messages gone

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,44 +1,55 @@
-// 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-// 2. Define your collection(s)
+// Blog collection
 const blogCollection = defineCollection({
+  type: "content",
   schema: z.object({
-    draft: z.boolean(),
     title: z.string(),
-    snippet: z.string(),
+    category: z.string().optional(),
+    author: z.string().optional(),
+    draft: z.boolean().default(false),
+    publishDate: z.coerce.date(),
+    snippet: z.string().optional(),
     image: z.object({
       src: z.string(),
       alt: z.string(),
-    }),
-    publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('Astroship'),
-    category: z.string(),
-    tags: z.array(z.string()),
-    test: z.string().optional(),
-    profilePic: z.object({
-      src: z.string(),
-      alt: z.string(),
     }).optional(),
+    tags: z.array(z.string()).optional(),
+    profilePic: z
+      .object({
+        src: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
   }),
 });
 
+
+
+// Team collection
 const teamCollection = defineCollection({
+  type: "content",
   schema: z.object({
-    draft: z.boolean(),
     name: z.string(),
     title: z.string(),
     avatar: z.object({
       src: z.string(),
       alt: z.string(),
     }),
-    publishDate: z.string().transform(str => new Date(str)),
+    draft: z.boolean().optional(),
+    publishDate: z.coerce.date().optional(),
+    bio: z.string().optional(),
+    position: z.string().optional(),
+    image: z.string().optional(),
   }),
 });
 
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
+// add this 👇
+const indexCollection = defineCollection({
+  type: "data",
+});
+
 export const collections = {
-  'blog': blogCollection,
-  'team': teamCollection,
+  blog: blogCollection,
+  team: teamCollection,
 };
